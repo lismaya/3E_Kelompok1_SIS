b0676028df74875bb652eac696e5e5def572a245
lismaya <lismayaok@gmail.com>
2019-12-10 06:43:30 +0000

    tampilan login dan logo sistem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,9 +26,10 @@
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <!-- <p class="login-box-msg">Sign in to start your session</p> -->
 
       <form action="../../index3.html" method="post">
+        <img src="{{asset('gambar/logo.png')}}" alt="">
         <div class="input-group mb-3">
           <input type="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
@@ -46,14 +47,14 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
+          <!-- <div class="col-8">
             <div class="icheck-primary">
               <input type="checkbox" id="remember">
               <label for="remember">
                 Remember Me
               </label>
             </div>
-          </div>
+          </div> -->
           <!-- /.col -->
           <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
@@ -61,7 +62,7 @@
           <!-- /.col -->
         </div>
       </form>
-
+<!--
       <div class="social-auth-links text-center mb-3">
         <p>- OR -</p>
         <a href="#" class="btn btn-block btn-primary">
@@ -70,12 +71,12 @@
         <a href="#" class="btn btn-block btn-danger">
           <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
         </a>
-      </div>
+      </div> -->
       <!-- /.social-auth-links -->
 
-      <p class="mb-1">
+      <!-- <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
-      </p>
+      </p> -->
       <p class="mb-0">
         <a href="register.html" class="text-center">Register a new membership</a>
       </p>
